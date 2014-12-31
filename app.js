620dba4855c5921c7e2b852bfe74b2f3c4920e28
rasbabbo <rasbabbo@yahoo.com>
2014-12-31 13:14:27 -0800

    added post button

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 var main = function () {
+	//menu hiding functions
 	$('.icon-menu').click(function() {
 		$('.menu').animate({left: '0px'}, 400);
 		$('body').animate({left: '285px'}, 400);
@@ -6,6 +7,13 @@ var main = function () {
 	$('.icon-close').click(function() {
 		$('.menu').animate({left: '-285px'}, 400);
 		$('body').animate({left: '0px'}, 400);
+	});
+
+	//COMMENTS SECTION
+	//button functionality
+	$('.btn').click(function() {
+		var post = $('.status-box').val();
+		$('<li>').text(post).prependTo('posts');
 	});
 };
 
