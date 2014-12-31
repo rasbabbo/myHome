ef81103d4df19ee95e2a35a6b6c7675500a7cb54
rasbabbo <rasbabbo@yahoo.com>
2014-12-31 13:18:17 -0800

    clear status box

--- app.js
+++ app.js
@@ -14,7 +14,11 @@ var main = function () {
 	$('.btn').click(function() {
 		var post = $('.status-box').val();
 		$('<li>').text(post).prependTo('posts');
+		//clear field
+		$('.status-box').val('');
 	});
+
+
 };
 
 $(document).ready(main);
